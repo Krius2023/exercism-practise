4df2773114cba7d0eebc5f130f591eacd4841117
Hello there! I am Krius here✨ <143637733+Krius2023@users.noreply.github.com>
2023-10-10 16:56:11 +0530

    my solution for darts exercise

---
+++ typescript/darts.ts
@@ -0,0 +1,16 @@
+// https://exercism.org/tracks/typescript/exercises/darts
+
+export function score(x: number, y: number): number {
+  let dart_position = ((x*x) + (y*y)) ** 0.5
+  switch (true) {
+    case dart_position <= 1:
+      return 10;
+    case dart_position <= 5:
+      return 5;
+    case dart_position <= 10:
+      return 1;
+    default:
+      break;
+  }
+  return 0;
+}
