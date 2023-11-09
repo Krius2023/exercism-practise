acb37f98235570c7853ada8d69d1918ab3428fd4
Hello there! I am Krius here✨ <143637733+Krius2023@users.noreply.github.com>
2023-11-09 08:09:49 +0530

    Update isogram.ts

--- typescript/isogram.ts
+++ typescript/isogram.ts
@@ -1,8 +1,15 @@
 // https://exercism.org/tracks/typescript/exercises/isogram
 
+// My first approch
 export function isIsogram(s: string): boolean {
   const str = s.toLowerCase().replace(/[-+.^:, ]/g,"");
   let modifiedStr = ([...new Set(str.split(''))].join('')).toString();
   let result = str === modifiedStr;
   return result;
+}
+
+// little bit changes in the appraoch
+export function isIsogram(s: string): boolean {
+  const str = s.toLowerCase().replace(/[^a-z]/g,"");
+  return new Set([...str]).size === str.length;
 }
